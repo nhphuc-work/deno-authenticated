fef8a4099ac21ef8321588d2161fd24ee3ab2897
d9pttuyen <d9pttuyen@cmcglobal.vn>
2022-10-27 15:30:29 +0700

    feat: init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
