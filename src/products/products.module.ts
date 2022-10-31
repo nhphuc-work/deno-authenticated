329c8dd5b264deb7e5bb482170d45afff372ab67
d9pttuyen <d9pttuyen@cmcglobal.vn>
2022-10-31 10:23:50 +0700

    feat: uploadfile engine

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
-    ConfigModule
+    ConfigModule,
+    MulterModule.register({
+      dest: './upload'
+    })
   ],
   controllers: [ProductsController],
   providers: [ProductsService]
