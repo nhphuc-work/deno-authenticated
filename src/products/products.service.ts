69c929e2ef68da752cdfc5a51a3ebd8660cca0d5
d9pttuyen <d9pttuyen@cmcglobal.vn>
2022-11-01 13:01:24 +0700

    feat: add docker-compose

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,18 @@
 import { Injectable, StreamableFile } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
 import { createReadStream } from 'fs';
 import { join } from 'path';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
   constructor(
-    private readonly configServices: ConfigService
+    private readonly configServices: ConfigService,
+    
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
   ) {}
 
   getFile(filename: string): StreamableFile {
@@ -23,5 +29,9 @@ export class ProductsService {
       originName: file?.originalname,
       name: file?.filename
     }
+  }
+
+  async getProducts() {
+    return await this.productRepository.find()
   }
 }
